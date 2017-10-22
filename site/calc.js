13fc13f4148d54bb1f82da5a1ec280d1ec282297
Alexandr Letyagin <aletyagin@radcor.local>
2017-10-23 01:39:52 +0300

    last one

--- site/calc.js
+++ site/calc.js
@@ -1,34 +1,33 @@
-var compnum = Math.round(Math.random() * 100),
-gameover = false;
+function message(text) {
+    alert('you have new massage!' + text);
+}
+message('you sic!');
 
-for (var i = 0; i < 10; i++) {
-for (var j = 0; j < 2; j++) {
-    var input = prompt('Раунд ' + (i + 1) + '\nИгрок ' + (j + 1) +
-        '\nВведите число\nВведите end для окончания игры');
-    if (input == 'end') {
-        gameover = true;
-        break;
+// функция определена сама по себе, видна всему документу. можно использовать и сверху и снизу её.
+var c = summ(34, 324);
+alert(c);
+function summ(a, b) {
+    return a + b;
+}
+
+// функция определена через переменную, использование возможно только после определения переменной
+var summ = function (a, b) {
+    return a+b;
+}
+alert(summ(343, 3432));
+
+// 4! = 1*2*3*4 = 24
+// 5! = 1*2*3*4*5 = 4!*5 = 120
+
+// factorial(5)
+// i=5, n=5
+// i=4, n=5*4
+// i=3, n=5*4*3
+alert(factorial(5));
+function factorial(i) {
+    var n = 1
+    while (i > 1) {
+        n *= i--;
     }
-    var usernum = +input;
-
-    if (usernum > compnum) {
-        alert('Число больше загаданного');
-    } else if (usernum < compnum) {
-        alert('Число меньше загаданного');
-    } else if (usernum == compnum) {
-        alert('Верный ответ!');
-        alert('Выиграл игрок ' + (j + 1) + '!\nОн угадал число с ' + (i + 1) + ' попытки.');
-        gameover = true;
-        break;
-    } else {
-        alert('Ввод должен быть числом от 1 до 100!');
-    }
-}
-if (gameover) {
-    break;
-}
-}
-if (i == 10) {
-alert('Игра окончена, Компьютер загадал число ' + compnum);
-
+    return n;
 }
